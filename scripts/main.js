899b60a07aaf5cd4d7a88381b5c9342abb900e57
Joaco Esteban <dev.joaco.esteban@gmail.com>
2019-11-29 11:58:49 -0300

    input

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,33 @@
 let gridContainer = document.getElementById('main-grid')
 
+
+window.addEventListener('keydown', function (event) {
+    let key = event.key
+    if (key !== 'ArrowUp' && key !== 'ArrowDown' && key !== 'ArrowLeft' && key !== 'ArrowRight') return
+    switch (key) {
+        case 'ArrowUp':
+            nextDirection = 0
+            break
+        case 'ArrowRight':
+            nextDirection = 1
+            break
+        case 'ArrowDown':
+            nextDirection = 2
+            break
+        case 'ArrowLeft':
+            nextDirection = 3
+            break
+    }
+})
+
+
 let rows = []
 let fruit = []
+let snakeCoords = [0, 0] 
+let direction = 1
+let nextDirection = null
+
+let moveTime = 75
 
 function createCells () {
     for (let i = 0; i < 25; i++) {
@@ -26,10 +52,62 @@ function createCells () {
 }
 
 function createFruit() {
-    fruit.row = Math.floor(Math.random() * 1000) % 25
-    fruit.cell = Math.floor(Math.random() * 1000) % 25
-    rows[fruit.row].cells[fruit.cell].classList.add('fruit')
+    fruit[0] = Math.floor(Math.random() * 1000) % 25
+    fruit[1] = Math.floor(Math.random() * 1000) % 25
+    rows[fruit[0]].cells[fruit[1]].classList.add('fruit')
 }
+
+function setSnake () {
+    rows[snakeCoords[0]].cells[snakeCoords[1]].classList.add('snake')
+}
+function clearSnake () {
+    rows[snakeCoords[0]].cells[snakeCoords[1]].classList.remove('snake')
+}
+
+function moveSnake () {
+    clearSnake()
+    let goTo = nextDirection
+    if (goTo === null) goTo = direction
+
+    console.log(direction, goTo)
+    switch (goTo) {
+        case 0:
+            if (direction === 2) goTo = direction
+            break
+        case 1:
+            if (direction === 3) goTo = direction
+            break
+        case 2:
+            if (direction === 0) goTo = direction
+            break
+        case 3:
+            if (direction === 1) goTo = direction
+            break
+    }
+
+    switch (goTo) {
+        case 0:
+            snakeCoords[0]--
+            break
+        case 1:
+            snakeCoords[1]++
+            break
+        case 2:
+            snakeCoords[0]++
+            break
+        case 3:
+            snakeCoords[1]--
+            break
+    }
+
+    nextDirection = null
+    direction = goTo
+    setSnake()
+}
+
+setInterval(moveSnake, moveTime)
 
 createCells()
 createFruit()
+setSnake()
+moveSnake()
