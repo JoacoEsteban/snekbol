8f4fccf38697bb3a3d4e29ef3f3ca0eec81abf4b
Joaco Esteban <dev.joaco.esteban@gmail.com>
2020-07-21 02:46:35 -0300

    ADD, client id

--- src/config/core.config.js
+++ src/config/core.config.js
@@ -4,7 +4,7 @@ const config = {
   ENV_PROD,
   BASE_URL: ENV_PROD ? (process.env.API_BASE_URL || 'snekbol-backend.herokuapp.com') : 'localhost:5000',
   USE_SSL: ENV_PROD,
-  GOOGLE_ADS_CLIENT: process.env.GOOGLE_ADS_CLIENT
+  GOOGLE_ADS_CLIENT: process.env.GOOGLE_ADS_CLIENT || 'ca-pub-2692027370405630'
 }
 
 export default config
