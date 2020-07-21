afc35979e016ca21c811a231b89d6e229fa83166
Joaco Esteban <dev.joaco.esteban@gmail.com>
2020-07-21 02:49:35 -0300

    FIX

--- src/controllers/GoogleAds.js
+++ src/controllers/GoogleAds.js
@@ -1,6 +1,6 @@
 import config from '../config/core.config'
 
 (() => {
-  if (!(config.ENV_PROD && config.AD_CLIENT)) return
+  if (!(config.ENV_PROD && config.GOOGLE_ADS_CLIENT)) return console.log('Not setting ads', config.ENV_PROD, config.GOOGLE_ADS_CLIENT)
   $('html head').append(`<script data-ad-client="${config.GOOGLE_ADS_CLIENT}" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>`)
 })()
