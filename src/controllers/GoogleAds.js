51e77a510a099e94ec56c3ae1b2db2971a4c3aa3
Joaco Esteban <dev.joaco.esteban@gmail.com>
2020-07-21 02:41:46 -0300

    FIX, script

--- src/controllers/GoogleAds.js
+++ src/controllers/GoogleAds.js
@@ -1,5 +1,6 @@
 import config from '../config/core.config'
 
-if (!(config.ENV_PROD && config.AD_CLIENT)) return
-
-$('html head').append(`<script data-ad-client="${config.GOOGLE_ADS_CLIENT}" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>`)
+(() => {
+  if (!(config.ENV_PROD && config.AD_CLIENT)) return
+  $('html head').append(`<script data-ad-client="${config.GOOGLE_ADS_CLIENT}" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>`)
+})()
